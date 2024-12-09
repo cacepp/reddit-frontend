fcb36d20fbabcf6314e43192f0cca25f87ee1f7d
cacepp <Iljushin.anton2002@yandex.ru>
2024-12-09 20:30:33 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import RedditHeader from '@/components/RedditHeader.vue'
+import RedditSidebar from '@/components/RedditSidebar.vue'
+
+
+</script>
+
+<template>
+  <div class="app-container">
+    <RedditHeader/>
+    <div class="app-container__main-content">
+      <RedditSidebar/>
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app-container {
+  font-family: "IBM Plex Sans Regular", sans-serif;
+  color: #EEF1F3;
+
+  &__main-content {
+    display: flex;
+    flex-direction: row;
+    height: calc(100vh - 57px);
+    max-height: calc(100vh - 57px);
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <span style="color: wheat">HOME-View</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
