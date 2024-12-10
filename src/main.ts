3b43b87782b04d8b1154f6f9c81ddf949eb6718e
cacepp <Iljushin.anton2002@yandex.ru>
2024-12-10 21:10:22 +0300

    feat: Добавлен vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
-import './assets/reset.scss'
-import './assets/fonts.scss'
-import './assets/main.scss'
+import '@/assets/reset.scss'
+import '@/assets/fonts.scss'
+import '@/assets/main.scss'
+import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app
+  .use(vuetify)
+  .use(createPinia())
+  .use(router)
 
 app.mount('#app')
